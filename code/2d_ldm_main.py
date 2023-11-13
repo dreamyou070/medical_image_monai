4d7a8cbe01fc6ef87b7b4ebcf76b8e540e7bb422
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-13 21:02:38 +0900

    Update 2d_ldm_main.py

--- code/2d_ldm_main.py
+++ code/2d_ldm_main.py
@@ -27,7 +27,7 @@ def get_transform(image_size) :
                                                                       rotate_range=[(-np.pi / 36, np.pi / 36), (-np.pi / 36, np.pi / 36)],
                                                                       translate_range=[(-1, 1), (-1, 1)],
                                                                       scale_range=[(-0.05, 0.05), (-0.05, 0.05)],
-                                                                      spatial_size=[image_size, image_size],
+                                                                      spatial_size=[160,84],
                                                                       padding_mode="zeros",
                                                                       prob=0.5,),])
     val_transforms = transforms.Compose([transforms.LoadImaged(keys=["image"]),
