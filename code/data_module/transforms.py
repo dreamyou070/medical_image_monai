fc6fcfcab1c36a2ed44b4c6a7aa95d12d135c07a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-13 21:30:43 +0900

    Update transforms.py

--- code/data_module/transforms.py
+++ code/data_module/transforms.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from monai.transforms.traits import LazyTrait
 from collections.abc import Callable
 from typing import Any, TypeVar
-from data.data_preprocessing import data_info_preprocess
+from data_module.data_preprocessing import data_info_preprocess
 
 ReturnType = TypeVar("ReturnType")
 __override_keywords = {"mode", "padding_mode", "dtype", "align_corners", "resample_mode", "device"}
