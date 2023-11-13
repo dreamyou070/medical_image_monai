e8682e4211f9b9017d81d7d785f13ab25929bd94
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-13 22:23:27 +0900

    Update autoencoder_inference.py

--- code/autoencoder_inference.py
+++ code/autoencoder_inference.py
@@ -36,7 +36,7 @@ def main(args) :
     for idx in random_idx :
         # ------------------------------------------------------------------------------------------------
         # org shape is [1615,840]
-        org_img = val_ds[idx]['image']
+        org_img = val_ds[idx]['image'].to(device)
         with torch.no_grad():
             recon_img, z_mu, z_sigma = autoencoderkl(org_img)
             print(f'random_idx : {random_idx} | recon_img.shape : {recon_img.shape}')
