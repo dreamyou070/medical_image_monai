47dbb6eef7a9a9df5b374ea54d5e1b7cf3f90fb7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-16 14:58:48 +0900

    Update autoencoder_training_idea.py

--- autoencoder_training_idea.py
+++ autoencoder_training_idea.py
@@ -92,10 +92,11 @@ def main(args):
             #normal_img_info = img_info[normal_index]
             #ood_img_info = img_info[ood_index]
 
-            mask_info = batch['mask'].to(img_info.dtype)
+            mask_info = batch['mask'].to(autoencoderkl.dtype)
             #normal_mask_info = mask_info[normal_index]
             #ood_mask_info = mask_info[ood_index]
             masked_img_info = img_info * mask_info.unsqueeze(1)
+            masked_img_info = masked_img_info.to(autoencoderkl.dtype)
 
             optimizer_g.zero_grad(set_to_none=True)
             with autocast(enabled=True):
