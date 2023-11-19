c92a34285865a7bc35f221872eedb0816ea1cee1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-20 08:42:26 +0900

    Update data_check.py

--- data_check.py
+++ data_check.py
@@ -5,32 +5,17 @@ from PIL import Image
 
 def main(args) :
 
-    #base_dir = r'../medical_Data/experiment/dental/panoramic_data_high'
-
-    print(f'\n step 1. image check')
-    img_base_dir = r'../medical_data/experiment/dental/panoramic_data_high/mask'
-    images = os.listdir(img_base_dir)
-
-    mask_base_dir = r'../medical_data/experiment/dental/panoramic_data_high/mask'
-    #mask_save_base_dir = os.path.join(base_dir, 'mask')
-    #os.makedirs(mask_save_base_dir, exist_ok=True)
-    for image_name in images :
-        image_dir = os.path.join(img_base_dir, image_name)
-        pil_image = Image.open(image_dir)
-        print(f'pil_image : {pil_image.size}')
-        #pil_image = pil_image.convert('L')
-        #pil_image = pil_image.resize((256, 256))
-        #pil_image.save(image_dir)
-        """
-        
-        
-
-        mask_dir = os.path.join(mask_base_dir, image_name)
-        pil_mask = Image.open(mask_dir)
-        pil_mask = pil_mask.convert('L')
-        pil_mask = pil_mask.resize((H,H))
-        pil_mask.save(os.path.join(mask_save_base_dir, f'{image_name}'))
-        """
+    base_folder = '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_256/original'
+    mask_folder = '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_256/mask'
+    images = os.listdir(base_folder)
+    for image in images :
+        image_dir = os.path.join(base_folder, image)
+        mask_dir = os.path.join(mask_folder, image)
+        pil = Image.open(image_dir)
+        try :
+            mask_pil = Image.open(mask_dir)
+        except :
+            print(f'{image} does not have mask')
 
 
 
