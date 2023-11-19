7246e98b044044c03d53baa573ea1010f0f4cbbb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-20 08:56:12 +0900

    data check second time

--- data_check.py
+++ data_check.py
@@ -5,18 +5,19 @@ from PIL import Image
 
 def main(args) :
 
-    base_folder = '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_256/original'
-    mask_folder = '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_256/mask'
+    base_folder = '../medical_data/experiment/dental/Radiographs'
     images = os.listdir(base_folder)
+    #mask_folder = '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_256/mask'
+
+    new_folder = '../medical_data/experiment/dental/panoramic_data_high/original'
+    os.makedirs(new_folder, exist_ok=True)
     for image in images :
         image_dir = os.path.join(base_folder, image)
-        mask_dir = os.path.join(mask_folder, image)
         pil = Image.open(image_dir)
-        try :
-            mask_pil = Image.open(mask_dir)
-        except :
-            print(f'{image} does not have mask')
-
+        pil = pil.resize((256, 256))
+        pil = pil.convert('L')
+        pil.save(os.path.join(new_folder, image))
+    """"""
 
 
 
