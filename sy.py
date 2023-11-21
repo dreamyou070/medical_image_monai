1f69e845d222b6a48da28a47d5b365ae37122e77
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-21 09:10:05 +0900

    Update sy.py

--- sy.py
+++ sy.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-base_folder = '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128'
+base_folder = '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train'
 folder_list = os.listdir(base_folder)
 save_folder = os.path.join('/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128', 'valid')
 os.makedirs(save_folder, exist_ok=True)
@@ -9,6 +9,7 @@ os.makedirs(save_folder, exist_ok=True)
 for folder in folder_list :
     folder_dir = os.path.join(base_folder, folder)
     new_folder_dir = os.path.join(save_folder, folder)
+    os.makedirs(new_folder_dir, exist_ok=True)
     images = os.listdir(folder_dir)
     for i, image in enumerate(images) :
         if i < 200 :
