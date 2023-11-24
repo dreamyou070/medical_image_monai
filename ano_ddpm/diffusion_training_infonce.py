d480d0b4b9dd4e1f133d3b4e1c2f086f2a439393
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-24 20:33:18 +0900

    Update diffusion_training_infonce.py

--- ano_ddpm/diffusion_training_infonce.py
+++ ano_ddpm/diffusion_training_infonce.py
@@ -117,8 +117,10 @@ def main(args):
         setproctitle('parksooyeon')
     print(f' (1.1) wandb')
     wandb.login(key=args.wandb_api_key)
-    wandb.init(project=args.wandb_project_name, name=args.wandb_run_name,
-               dir=args.experiment_dir)
+    wandb.init(project=args.wandb_project_name, name=args.wandb_run_name,)
+    os.environ["WANDB_DATA_DIR"] = args.experiment_dir
+
+
 
     print(f' (1.2) seed and device')
     seed(args.seed)
