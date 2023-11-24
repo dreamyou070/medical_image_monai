ff5c29dbfb989a47076e450b3c963fb3240bf24f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-24 23:19:14 +0900

    get wandb dir

--- ano_ddpm/vae_training.py
+++ ano_ddpm/vae_training.py
@@ -22,6 +22,7 @@ def main(args) :
     else:
         setproctitle('parksooyeon')
     print(f' (1.1) wandb')
+    os.environ = args.experiment_dir
     print(env.get_data_dir())
     """
     wandb.login(key=args.wandb_api_key)
