eaea39a03e4da69bd389be6817be0d2cf5f7b3a1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-24 18:16:25 +0900

    Update diffusion_training_infonce.py

--- ano_ddpm/diffusion_training_infonce.py
+++ ano_ddpm/diffusion_training_infonce.py
@@ -221,7 +221,7 @@ def main(args):
                 pos_loss_ = torch.nn.functional.mse_loss((noise_pred * mask_info.to(device)).float(),
                                                          (target * mask_info.to(device)).float(),
                                                          reduction="none").mean([1, 2, 3])
-                print(f'positive loss : {pos_loss_}')
+                print(f'positive loss : {pos_loss_} ')
                 pixel_num = mask_info.sum([1, 2, 3]).float().to(device)
                 pixel_num = torch.where(pixel_num == 0, 1, pixel_num)
                 pos_loss = pos_loss_ / pixel_num
