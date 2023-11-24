70e270e6d46dbf65ab3e9602b10934627d44e5c9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-24 09:07:33 +0900

    Update generate_heatmap.sh

--- ano_ddpm/generate_heatmap.sh
+++ ano_ddpm/generate_heatmap.sh
@@ -1,6 +1,14 @@
 device='cuda:3'
 experiment_dir='/data7/sooyeon/medical_image/anoddpm_result/7_gaussian_linear_pos_infonce'
 img_size='128,128'
+train_data_folder='/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/original'
+train_mask_dir='/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/mask'
+val_data_folder='/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/valid/original'
+val_mask_dir='/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/valid/mask'
 python generate_heatmap.py --device=${device} \
                            --experiment_dir=${experiment_dir} \
-                           --img_size=${img_size}
+                           --img_size=${img_size} \
+                           --train_data_folder=${train_data_folder} \
+                           --train_mask_dir=${train_mask_dir} \
+                           --val_data_folder=${val_data_folder} \
+                           --val_mask_dir=${val_mask_dir}
