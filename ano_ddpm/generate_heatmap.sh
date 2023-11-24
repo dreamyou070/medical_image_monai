1004c95463f37b82ce6d691d576adabe5384090c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-24 15:37:34 +0900

    new epoch

--- ano_ddpm/generate_heatmap.sh
+++ ano_ddpm/generate_heatmap.sh
@@ -1,5 +1,5 @@
 device='cuda:3'
-model_name='unet_epoch_300.pt'
+model_name='unet_epoch_250.pt'
 experiment_dir='/data7/sooyeon/medical_image/anoddpm_result/7_gaussian_linear_pos_infonce'
 img_size='128,128'
 train_data_folder='/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/original'
