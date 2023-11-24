548075c60ddc04ee77450675f2a0e23a063236d7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-24 09:05:28 +0900

    Update generate_heatmap.sh

--- ano_ddpm/generate_heatmap.sh
+++ ano_ddpm/generate_heatmap.sh
@@ -1,5 +1,6 @@
 device='cuda:3'
 experiment_dir='/data7/sooyeon/medical_image/anoddpm_result/7_gaussian_linear_pos_infonce'
-
+img_size='128,128'
 python generate_heatmap.py --device=${device} \
-                           --experiment_dir=${experiment_dir}
+                           --experiment_dir=${experiment_dir} \
+                           --img_size=${img_size}
