92467f04a559f44991af1b122689c60c187c1c55
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-25 12:45:26 +0900

    Update unet_training.sh

--- dental_ldm/unet_training.sh
+++ dental_ldm/unet_training.sh
@@ -5,4 +5,4 @@ python unet_training.py --wandb_project_name 'test' --wandb_run_name 'ldm_traini
                         --train_mask_dir '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/mask' \
                         --val_data_folder '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/valid/original' \
                         --val_mask_dir '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/valid/mask' \
-                        --img_size '128,128' --batch_size 6
+                        --img_size '128,128' --batch_size 6 --masked_loss
