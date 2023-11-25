a17bc05469ef97f07b310f0923cb93118a9c9fec
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-26 00:44:44 +0900

    Update unet_ldm_pipeline_training.py

--- dental_ldm/unet_ldm_pipeline_training.py
+++ dental_ldm/unet_ldm_pipeline_training.py
@@ -95,7 +95,7 @@ def training_outputs(args, test_data, scheduler, is_train_data, device, model, v
         real = torch_transforms.ToPILImage()(real.unsqueeze(0))
         # wrong in here ...
         recon = recon_image[img_index].squeeze()
-        recon = (recon / 2 + 0.5).clamp(0, 1).unsqueeze(0)
+        recon = (recon / 2 + 0.5).clamp(0, 1).unsqueeze(0).unsqueeze(0)
         recon = recon.cpu().permute(0, 2, 3, 1).numpy()
         recon = (recon * 255).astype(np.uint8)
         print(f'recon : {recon}')
