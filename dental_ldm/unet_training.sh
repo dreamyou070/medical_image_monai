1eb00bdd3fb939afa915761753a21d386eb904da
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-25 13:29:47 +0900

    update code

--- dental_ldm/unet_training.sh
+++ dental_ldm/unet_training.sh
@@ -8,17 +8,8 @@ python unet_training.py --wandb_project_name 'anoddpm_result_ldm' --wandb_run_na
                         --model_save_freq 50 --img_size '128,128' --batch_size 6 --masked_loss
 
 [screen 01]
-python unet_training.py --wandb_project_name 'anoddpm_result_ldm' --wandb_run_name '2_info_nce_loss' --device 'cuda:1' \
-                        --experiment_dir '/data7/sooyeon/medical_image/anoddpm_result_ldm/2_info_nce_loss' \
-                        --train_data_folder '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/original' \
-                        --train_mask_dir '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/mask' \
-                        --val_data_folder '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/valid/original' \
-                        --val_mask_dir '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/valid/mask' \
-                        --model_save_freq 50 --img_size '128,128' --batch_size 6 --info_nce_loss
-
-[screen 02]
-python unet_training.py --wandb_project_name 'anoddpm_result_ldm' --wandb_run_name '3_pos_info_nce_loss' --device 'cuda:2' \
-                        --experiment_dir '/data7/sooyeon/medical_image/anoddpm_result_ldm/3_pos_info_nce_loss' \
+python unet_training.py --wandb_project_name 'anoddpm_result_ldm' --wandb_run_name '2_pos_info_nce_loss' --device 'cuda:1' \
+                        --experiment_dir '/data7/sooyeon/medical_image/anoddpm_result_ldm/2_pos_info_nce_loss' \
                         --train_data_folder '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/original' \
                         --train_mask_dir '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/train/mask' \
                         --val_data_folder '/data7/sooyeon/medical_image/experiment_data/dental/panoramic_data_res_128/valid/original' \
