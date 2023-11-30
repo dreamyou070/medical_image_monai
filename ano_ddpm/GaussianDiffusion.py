080fbe1681e235e90271d9f7c9c8d3c6cadc1049
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-11-30 10:38:51 +0900

    Update GaussianDiffusion.py

--- ano_ddpm/GaussianDiffusion.py
+++ ano_ddpm/GaussianDiffusion.py
@@ -307,9 +307,7 @@ class GaussianDiffusionModel:
                  - 'output': a shape [N] tensor of NLLs or KLs.
                  - 'pred_xstart': the x_0 predictions.
         """
-        true_mean, _, true_log_variance_clipped = self.q_posterior_mean_variance(x_start=x_start,
-                                                                                 x_t=x_t,
-                                                                                 t=t)
+        true_mean, _, true_log_variance_clipped = self.q_posterior_mean_variance(x_start,x_t,t)
         out = self.p_mean_variance(model, x_t, t, clip_denoised=clip_denoised, model_kwargs=model_kwargs)
         kl = normal_kl(true_mean, true_log_variance_clipped, out["mean"], out["log_variance"])
         kl = mean_flat(kl) / np.log(2.0)
