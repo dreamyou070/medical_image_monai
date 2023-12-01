20a6bf686f4422348b0237c7c9c208e58c85ed6d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-01 23:13:21 +0900

    Update respace.py

--- ano_ddpm/ImprovedDDPM/improved_diffusion/respace.py
+++ ano_ddpm/ImprovedDDPM/improved_diffusion/respace.py
@@ -115,11 +115,9 @@ class _WrappedModel:
         self.original_num_steps = original_num_steps
 
     def __call__(self, x, ts, **kwargs):
-        map_tensor = th.tensor(self.timestep_map,
-                               #device=ts.device,
-                               device = self.model.device,
-                               dtype=ts.dtype)
-        new_ts = map_tensor[ts] 
+        map_tensor = th.tensor(self.timestep_map, #device=ts.device,
+                               device = self.model.device, dtype=ts.dtype)
+        new_ts = map_tensor[ts].to(self.model.device) 
         if self.rescale_timesteps:
             new_ts = new_ts.float() * (1000.0 / self.original_num_steps)
         return self.model(x, new_ts, **kwargs)
