6f0a7e1248a93d03f6f9150353a749f412357baa
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-02 09:22:23 +0900

    Update train_main.py

--- improved_ldm/train_main.py
+++ improved_ldm/train_main.py
@@ -385,7 +385,7 @@ if __name__ == "__main__":
     parser.add_argument('--weight_decay', type=float, default=0.0)
     parser.add_argument('--lr_anneal_steps', type=int, default=0)
 
-    parser.add_argument('--batch_size', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=2)
     parser.add_argument('--microbatch', type=int, default=-1)
 
 
